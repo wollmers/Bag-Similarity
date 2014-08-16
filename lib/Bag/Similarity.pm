a12fa491f0c575291b06189c4b3d33253dca1c7a
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-08-16 07:43:12 +0200

    add travis

--- lib/Bag/Similarity.pm
+++ lib/Bag/Similarity.pm
@@ -93,6 +93,11 @@ __END__
 
 Bag::Similarity - Similarity measures for bags
 
+=for html
+
+<a href="https://travis-ci.org/wollmers/Bag-Similarity"><img src="https://travis-ci.org/wollmers/Bag-Similarity.png" alt="Bag-Similarity"></a>
+<a href='https://coveralls.io/r/wollmers/Bag-Similarity?branch=master'><img src='https://coveralls.io/repos/wollmers/Bag-Similarity/badge.png?branch=master' alt='Coverage Status' /></a>
+
 =head1 SYNOPSIS
 
   use Bag::Similarity;
